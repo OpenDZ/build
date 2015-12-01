a4d290c9e258f04402808e036aed867f458102cb
Kay Sievers <kay@vrfy.org>
2015-12-01 23:42:08 +0100

    add bus1 binaries to system image

--- src/install-fedora.sh
+++ src/install-fedora.sh
@@ -7,6 +7,12 @@ test "$UID" == "0" || exit 1
 
 # ------------------------------------------------------------------------------
 ROOT=$(mktemp -d /tmp/install-tmpXXX)
+SYSTEM=$(mktemp -d system-tmpXXX)
+
+for i in dev sys run proc; do
+    mkdir $ROOT/$i
+    mount --bind /$i $ROOT/$i
+done
 
 # install rawhide packages
 dnf -y --nogpg \
@@ -30,24 +36,19 @@ ln -s lib/x86_64-linux-gnu $ROOT/usr/lib64
 $ROOT/usr/sbin/ldconfig -r $ROOT
 
 # copy usr (without the packages the kernel package pulls in)
-cp -axl $ROOT/usr $ROOT/system
-rm -f $ROOT/system/lib64
+cp -ax $ROOT/usr/* $SYSTEM
+rm -f $SYSTEM/lib64
 
 # copy etc into usr
-cp -axl $ROOT/etc $ROOT/system/
+cp -ax $ROOT/etc $SYSTEM
 
 # merge sbin into bin
-mv --no-clobber $ROOT/system/sbin/* $ROOT/system/bin
-rm -rf $ROOT/system/sbin
-ln -s bin $ROOT/system/sbin
+mv --no-clobber $SYSTEM/sbin/* $SYSTEM/bin
+rm -rf $SYSTEM/sbin
+ln -s bin $SYSTEM/sbin
 
 # delete cruft
-rm -rf $ROOT/system/{tmp,games,local}
-
-# keep bash happy
-for i in dev sys run proc; do
-    mount --bind /$i $ROOT/$i
-done
+rm -rf $SYSTEM/{tmp,games,local}
 
 # ------------------------------------------------------------------------------
 # install kernel
@@ -58,15 +59,15 @@ dnf -y --nogpg \
   --exclude grubby \
   kernel
 
+# copy kernel and firmware to usr
+mv $ROOT/usr/lib/modules $SYSTEM/lib
+mv $ROOT/usr/lib/firmware $SYSTEM/lib
+
+# ------------------------------------------------------------------------------
 for i in dev sys run proc; do
     umount $ROOT/$i
 done
 
-# copy kernel and firmware to usr
-mv $ROOT/usr/lib/modules $ROOT/system/lib
-mv $ROOT/usr/lib/firmware $ROOT/system/lib
-
-# ------------------------------------------------------------------------------
-rm -f system.img
-mksquashfs $ROOT/system system.img
 rm -rf $ROOT
+rm -rf system
+mv $SYSTEM system
